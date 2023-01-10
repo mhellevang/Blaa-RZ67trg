c5b5a44565694418500423bc9dbdb6dce6fbfbae
Mathias Hellevang <mathias.hellevang@gmail.com>
2023-01-10 22:01:36 +0100

    Test of BLE version

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,92 @@
 #include <Arduino.h>
-#include "BluetoothSerial.h"
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define SERVICE_UUID        "c9239c9e-6fc9-4168-b3aa-53105eb990b0"
+#define CHARACTERISTIC_UUID "458d4dc9-349f-401d-b092-a2b1c55f5319"
 
 // the delay between button on and button off
 #define DELAY 1000
 
-int led_pin_1 = 4; // TTGO-Display led
+const int led_pin_1 = 4; // TTGO-Display led
+const int shutterPin = 23; // Pin for trigger
 
 int incoming;
 unsigned long now;
 unsigned long timestampButton1;
 
-const int shutterPin = 23;
+// BLE
+bool deviceConnected = false;
 
-BluetoothSerial ESP_BT;
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer *pServer) {
+        deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer *pServer) {
+        deviceConnected = false;
+    };
+
+};
+
+class MyCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+        now = millis(); // Store current time
+        std::string value = pCharacteristic->getValue();
+
+        Serial.print("Value: ");
+        Serial.println(value.c_str());
+        try {
+            Serial.println("Trying to convert");
+            char c = value.c_str()[0];
+            if (c == '1') {
+                // TODO: Not completely sure if this is the right way to do it
+                digitalWrite(shutterPin, HIGH);
+                delay(100);
+                digitalWrite(shutterPin, LOW);
+            }
+        } catch (const std::exception &e) {
+            Serial.println("Error");
+        }
+
+
+        unsigned long newTime = millis();
+        unsigned long diff = newTime - now;
+        Serial.print("Time to process: ");
+        Serial.print(diff);
+        Serial.println("ms");
+
+    }
+};
 
 void setup() {
     Serial.begin(19200);
-    ESP_BT.begin("RZ67 Blaa");
+    // Serial.begin(115200);
+    Serial.println("Starting BLE work!");
+
+    BLEDevice::init("RZ67 Blaa");
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+            CHARACTERISTIC_UUID,
+            BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE
+    );
+    pCharacteristic->setCallbacks(new MyCallbacks());
+
+    pCharacteristic->setValue("Hello World says Neil");
+    pService->start();
+
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+    pAdvertising->setMinPreferred(0x12);
+    BLEDevice::startAdvertising();
+    Serial.println("Characteristic defined! Now you can read it in your phone!");
 
     // Digital buttons from app
     pinMode(led_pin_1, OUTPUT);
@@ -25,59 +95,13 @@ void setup() {
     pinMode(shutterPin, OUTPUT);
 }
 
-void openShutter() {
-    digitalWrite(shutterPin, HIGH);
-}
-
-void closeShutter() {
-    digitalWrite(shutterPin, LOW);
-}
-
-void triggerShutter() {
-    Serial.println("Trigger shutter");
-    openShutter();
-    delay(100);
-    closeShutter();
-}
-
-void handleDigitalTriggers() {
-    now = millis(); // Store current time
-    if (ESP_BT.available()) {
-        incoming = ESP_BT.read(); //Read what we receive
-
-        // separate button ID from button value -> button ID is 10, 20, 30, etc, value is 1 or 0
-        int button = floor(incoming / 10);
-        int value = incoming % 10;
-
-        switch (button) {
-            case 1:
-                Serial.print("Button 1:");
-                Serial.println(value);
-                digitalWrite(led_pin_1, value); // Indicate in led
-                if(value == 1) // check if the button is switched on
-                    timestampButton1 = now; // if button is switched on, write the current time to the timestamp
-                triggerShutter();
-                break;
-            case 2:
-                Serial.print("Button 2:");
-                Serial.println(value);
-                break;
-            case 3:
-                Serial.print("Button 3:");
-                Serial.println(value);
-                break;
-        }
-    }
-
-    // time-out check -> check if a button is on and whether it needs to be switched off
-    if(digitalRead(led_pin_1) and now > timestampButton1 + DELAY) { // if output port (led_pin_1) is active and if the delay time has passed, the button is to be switched off
-        digitalWrite(led_pin_1, 0); // set output port to 0
-        ESP_BT.write(10); // send byte to phone indicateding that Button 1 is to be set to 0 -> 10
-        Serial.print("Button 1 timeout - value: 0"); // write to serial port for easy debugging
-    }
-}
 
 void loop() {
-    handleDigitalTriggers();
+    // put your main code here, to run repeatedly:
+    if (deviceConnected) {
+        digitalWrite(led_pin_1, HIGH);
+    } else {
+        digitalWrite(led_pin_1, LOW);
+    }
 }
 
